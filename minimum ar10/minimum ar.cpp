348aa8af3c92733cc6ae810941ef8cfe6a83ef87
KRIDSAYA NATEEMONTON <63010026@KMITL.AC.TH>
2020-08-27 22:23:14 +0700

    Add project files.

---
+++ minimum ar10/minimum ar.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	int num[10];
+	int n;
+	int tmp;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("Enter number %d : ", n + 1);
+		scanf_s("%d", &num[n]);
+	}
+	for (n = 0; n < 10; n++)
+	{
+		if (num[n] < num[n + 1])
+		{
+			tmp = num[n + 1];
+			num[n + 1] = num[n];
+			num[n] = tmp;
+		}
+	}
+	printf("The minimum number = %d", num[9]);
+	return 0;
+}
